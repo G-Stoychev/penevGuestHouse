3f005a8566f9f8a6293cf353d0d5202fe8ce1379
JoroTop <GStoychev20@gmail.com>
2025-12-26 12:11:03 +0200

    fix bug render Main

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,15 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-function Main() {
-    const [darkMode, setDarkMode] = useState(false);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-    useEffect(() => {
-        const html = document.documentElement;
-        if (darkMode) html.classList.add("dark");
-        else html.classList.remove("dark");
-    }, [darkMode]);
-
-    ReactDOM.createRoot(document.getElementById("root")).render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <App darkMode={darkMode} setDarkMode={setDarkMode} />;
-            </BrowserRouter>
-        </React.StrictMode>
-    );
-}
-
-ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>
+);
